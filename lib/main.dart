eeac199f203e649107e70069fd838f27bd98393c
Wolfram-180 <sergei.ulvis@gmail.com>
2024-02-27 17:30:46 +0300

    imports set

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:bloc_firebase_gallery/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       title: 'Test app',
